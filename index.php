6baa5a605464e05e59ac60a48f953540452f3ccd
Ali Khorsandfard <secure73@gmail.com>
2024-11-20 11:16:50 +0100

    login form

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php require_once('./layout/header.php'); ?>
 
-        Hello from html
+<h2> post Test</h2>
+<hr>
+<h2>Login</h2>
 
-<?php require_once('./layout/footer.php');?>  
+<section>
+    <form class="form">
+
+        <input type="text" name="xuser" class="input input-field mb-2" placeholder="Benutzername">
+        <input type="password" name="xpass" class="input-field mb-2" placeholder="Passwort">
+        <br>
+        <button type="submit" class="btn btn-sm btn-primary">Einloggen</button>
+    </form>
+    <div class="signup-link">
+        <p>Noch kein Konto? <a>Jetzt registrieren</a></p>
+    </div>
+</section>
+
+<!-- section result !-->
+<section>
+    <p>
+        hire ist php action result: 
+        <br>
+        <span>????</span>
+    </p>
+</section>
+
+
+<?php require_once('./layout/footer.php'); ?>
